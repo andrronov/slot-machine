eee16db56abfd810a850583472cddedc551963be
andrronov <andrew270504cska@gmail.com>
2026-05-24 10:02:56 +0700

    chore: update deps, add proper eslint & prettier config

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,40 @@
+import skipFormatting from "@vue/eslint-config-prettier/skip-formatting";
+import vueTsEslintConfig from "@vue/eslint-config-typescript";
+import eslintPluginImportX from "eslint-plugin-import-x";
+import pluginVue from "eslint-plugin-vue";
 import baseConfig from "./codestyle/config-eslint/base.js";
 
 export default [
   {
     name: "app/files-to-lint",
-    files: ["**/*.{js,vue,ts}"],
+    files: ["**/*.{ts,mts,tsx,vue}"],
   },
 
-  {
-    name: "app/files-to-ignore",
-    ignores: ["**/dist/**"],
-  },
+  ...pluginVue.configs["flat/strongly-recommended"],
+  ...vueTsEslintConfig(),
 
   {
+    plugins: {
+      "import-x": eslintPluginImportX,
+    },
+
     rules: {
       ...baseConfig.rules,
+
+      "no-unused-expressions": "off",
+      "max-lines": "off",
+      "sort-imports": "off",
+
+      "@typescript-eslint/no-unused-expressions": "off",
+      "@typescript-eslint/no-implicit-any": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
+
+      "import-x/no-self-import": "error",
+      "import-x/no-cycle": "error",
+      "vue/multi-word-component-names": "off",
+      "vue/no-unused-emit-declarations": "error",
     },
   },
+
+  skipFormatting,
 ];
